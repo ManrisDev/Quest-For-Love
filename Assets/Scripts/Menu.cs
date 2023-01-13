4877b72c5acd76976e889e3dc55a6ed8c1749576
Grumaboy <grumak.e8913@gmail.com>
2023-01-13 21:44:45 +0300

    Menu + LoveBar

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject menuPanel;
+    [SerializeField] GameObject settingsPanel;
+
+    public void StartGame()
+    {
+        Debug.Log("Старт");
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Настройки");
+        menuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Выход");
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        menuPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+}
