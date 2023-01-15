154f413c48aa8e868659298131a118a2e21008bf
Grumaboy <grumak.e8913@gmail.com>
2023-01-15 14:07:50 +0300

    VolumeSettings

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField] GameObject menuPanel;
-    [SerializeField] GameObject settingsPanel;
+    public AudioMixer audioMixer;
+    public float volumevalue;
+    public Slider volumeslider;
 
+    private void Start()
+    {
+        volumeslider.value = PlayerPrefs.GetFloat("volume");
+    }
+
+    private void Update()
+    {
+        audioMixer.SetFloat("volume", volumevalue);
+        PlayerPrefs.SetFloat("volume", volumevalue);
+    }
     public void StartGame()
     {
         Debug.Log("Старт");
@@ -15,8 +28,6 @@ public class Menu : MonoBehaviour
     public void Settings()
     {
         Debug.Log("Настройки");
-        menuPanel.SetActive(false);
-        settingsPanel.SetActive(true);
     }
 
     public void Quit()
@@ -25,9 +36,13 @@ public class Menu : MonoBehaviour
         Application.Quit();
     }
 
-    public void Back()
+    public void SetVolume(float volume)
     {
-        menuPanel.SetActive(true);
-        settingsPanel.SetActive(false);
+        volumevalue = volume;
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
     }
 }
